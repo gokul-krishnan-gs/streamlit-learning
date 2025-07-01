b4e6324e34cd02859f3972aed077a74e0c18efd9
Gokul Krishnan <gokulkrishnangs02@gmail.com>
2025-07-01 12:29:53 +0530

    Add files via upload

---
+++ day1/hello.py
@@ -0,0 +1,19 @@
+
+
+
+#importing the streamlit library and alias as "st" for easy acces
+import streamlit as st
+
+#title() is used to set main heading of your page
+# we can use emojis to look fun
+st.title("Hello World!")
+
+#add a smaller header below the title
+st.subheader("This my web page")
+
+#this is most flexible function,it can display text,variables,even plot charts
+st.write("this is super easyyyyyyyyyyyy")
+
+#shows a green success box, often used to show confirmation or a positive message.
+st.success("You are learning Gokul Krishnan!!!!!!")
+
